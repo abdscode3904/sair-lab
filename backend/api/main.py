823ec1c459b5cd4a75bd75646566318ae436b0dd
Abdul Bari <abdulbarisada@gmail.com>
2026-09-25 18:19:31 +0530

    Fix CORS for frontend

--- backend/api/main.py
+++ backend/api/main.py
@@ -133,13 +133,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        # Local development
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://[::1]:5500",
-
-        # Render API / future public frontend support
-        "https://sair-lab.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
